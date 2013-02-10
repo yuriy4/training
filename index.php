381e6661a77196469f10540bd2a797653fc0a783
yuriy4 <usteo@gmail.ru>
2013-02-10 21:09:19 +0200

    ficsion

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo "Hello, world! This is my first PHP project!";
+        ?>
+    </body>
+</html>
